ba5650a07357dde2eeb019a7284ff613e97c964a
Saimon Moore <saimonmoore@gmail.com>
2016-08-31 11:44:28 +0200

    Adding ruby version

--- apps/quicksort/lib/quicksort.ex
+++ apps/quicksort/lib/quicksort.ex
@@ -1,3 +1,17 @@
+# c.f ruby version:
+#
+# class QuickSort
+#   def sort(list)
+#     return [] if list.empty?
+#     pivot, rest = list.first, list[1..-1]
+#
+#     left, right = *rest.partition {|e| e < pivot}
+#     sort(left).concat([pivot].concat(sort(right)))
+#   end
+# end
+#
+# Both are pretty readable but the elixir version looks more elegant.
+
 defmodule Quicksort do
   def sort([]), do: []
   def sort([pivot | rest]) do
